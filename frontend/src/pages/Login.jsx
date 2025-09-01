91080fbe101f555717b4d0e52bc40c0944c9cf45
Mayur das vaishnav <109586835+mayurvaishnav9977@users.noreply.github.com>
2025-09-01 23:02:26 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ function Login(){
 
             }
             try{
-                const url = "http://localhost:8080/auth/login"
+                const url = "https://expense-tracker-mern-sigma.vercel.app/auth/login"
                 const response = await fetch(url,{
                     method: "POST",
                     headers:{
